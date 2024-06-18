1ff0a1884f5794043a99de3f08fffc59920f0d8b
Kirula Nanayakkara <84916544+kirula0626@users.noreply.github.com>
2024-06-18 22:54:56 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,6 +4,10 @@
 test=$(netstat -tulpn | grep 9092 | cut -d "/" -f 1 | cut -d "N" -f 2 )
 kill -9 $test;
 
+#-Main tunnel
+python main.py &
+sleep 3
+
 #-Auto login with password
 #sshpass -p [SERVER_SSH_PASSWORD] ssh -C -o "ProxyCommand=nc -X CONNECT -x 127.0.0.1:9092 %h %p" [SERVER_USERNAME]@[SERVER_PUBLIC_IP] -p 443 -v -CND 1080 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
 
